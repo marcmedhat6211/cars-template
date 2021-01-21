69deae12c85745dad926ba68e52f91f018545001
marcmedhat6211 <marcmedhat6211@gmail.com>
2021-01-21 02:44:11 +0200

    pushing to main

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict";
+
+    /**SLIDER USING SWIPER**/
+	var cardSlider = new Swiper('.card-slider', {
+        loop: true,
+        navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+		slidesPerView: 1.85,
+        spaceBetween: 30,
+        centeredSlides: true,
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 200,
+            },
+              768: {
+                slidesPerView: 1,
+                spaceBetween: 40,
+            },
+              1024: {
+                slidesPerView: 1.7,
+                spaceBetween: 30,
+            },
+        }
+    });
+
+})(jQuery);
